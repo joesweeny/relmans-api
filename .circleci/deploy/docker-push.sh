7edae8bafc94b7d3612fe3b84ac1ac789bd3e2b0
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-13 11:14:43 +0000

    Update docker image references

--- .circleci/deploy/docker-push.sh
+++ .circleci/deploy/docker-push.sh
@@ -4,5 +4,5 @@ set -e
 
 aws ecr get-login --no-include-email --region $AWS_DEFAULT_REGION | bash
 
-docker tag "relmansapi_api" "$AWS_ECR_ACCOUNT_URL/relmans-api:$CIRCLE_SHA1"
+docker tag "relmans-api_api" "$AWS_ECR_ACCOUNT_URL/relmans-api:$CIRCLE_SHA1"
 docker push "$AWS_ECR_ACCOUNT_URL/relmans-api:$CIRCLE_SHA1"
