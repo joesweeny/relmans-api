7744f5cc03bf80ae9ca62545a8a6c451357955c8
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-16 11:15:44 +0000

    Update measurement enum

--- src/app/Domain/Enum/Measurement.php
+++ src/app/Domain/Enum/Measurement.php
@@ -5,25 +5,19 @@ namespace Relmans\Domain\Enum;
 use MyCLabs\Enum\Enum;
 
 /**
- * @method static Measurement BAG()
- * @method static Measurement BUNCH()
  * @method static Measurement EACH()
  * @method static Measurement GRAMS()
  * @method static Measurement HALF()
  * @method static Measurement KILOGRAMS()
- * @method static Measurement PACK()
- * @method static Measurement PUNNET()
+ * @method static Measurement LITRE()
  * @method static Measurement TRAY()
  */
 class Measurement extends Enum
 {
-    private const BAG = 'BAG';
-    private const BUNCH = 'BAG';
     private const EACH = 'EACH';
     private const GRAMS = 'GRAMS';
     private const HALF = 'HALF';
     private const KILOGRAMS = 'KILOGRAMS';
-    private const PACK = 'PACK';
-    private const PUNNET = 'PUNNET';
+    private const LITRE = 'LITRE';
     private const TRAY = 'TRAY';
 }
