bbe4178549f57c05173462ea0a1af543db63495a
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-02-24 15:47:32 +0000

    Add route to mapper

--- src/app/Framework/Routing/RouteMapper.php
+++ src/app/Framework/Routing/RouteMapper.php
@@ -3,6 +3,7 @@
 namespace Relmans\Framework\Routing;
 
 use Relmans\Application\Http\CreateCategoryController;
+use Relmans\Application\Http\CreateOrderController;
 use Relmans\Application\Http\CreateProductController;
 use Relmans\Application\Http\DeleteProductController;
 use Relmans\Application\Http\GetOrderController;
@@ -21,11 +22,14 @@ class RouteMapper
         $app->get('/health', HealthCheckController::class);
         $app->post('/category', CreateCategoryController::class);
         $app->get('/category', ListCategoriesController::class);
+
         $app->post('/product', CreateProductController::class);
         $app->get('/product', ListProductsController::class);
         $app->delete('/product/{id}', DeleteProductController::class);
         $app->patch('/product/{id}', UpdateProductController::class);
         $app->patch('/price/{id}', UpdateProductPriceController::class);
+
+        $app->post('/order', CreateOrderController::class);
         $app->get('/order', ListOrdersController::class);
         $app->get('/order/{id}', GetOrderController::class);
     }
