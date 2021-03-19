b90359bd10452f8cae73e43d186b4d0884a3a398
Joe Sweeny <joesweeny@intelligencefusion.co.uk>
2021-03-19 18:13:12 +0000

    Add cancelled order status

--- src/app/Domain/Enum/OrderStatus.php
+++ src/app/Domain/Enum/OrderStatus.php
@@ -6,12 +6,14 @@ use MyCLabs\Enum\Enum;
 
 /**
  * @method static OrderStatus ACCEPTED()
+ * @method static OrderStatus CANCELLED()
  * @method static OrderStatus PAYMENT_RECEIVED()
  * @method static OrderStatus PENDING()
  */
 class OrderStatus extends Enum
 {
     private const ACCEPTED = 'ACCEPTED';
+    private const CANCELLED = 'CANCELLED';
     private const PAYMENT_RECEIVED = 'PAYMENT_RECEIVED';
     private const PENDING = 'PENDING';
 }
